2bff724e9bffc3d42e313b7737ef5e1bc35abab0
jiyoung yun <letea8018@gmail.com>
2023-03-11 20:59:33 +0900

    todolist-js

--- main.js
+++ main.js
@@ -1,47 +1,57 @@
-// 유저는 할일을 추가할 수 있다.
-// 각 할일에 삭제와 체크버튼이 있다.
-
-
-// 끝난 할일은 되돌리기 버튼을 클릭하면 다시 되돌릴 수 있다.
-// 탭을 이용해 아이템들을 상태별로 나누어서 볼 수 있다.
-// 모바일 버전에서도 확인할 수 있는 반응형 웹이다
-
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
 let taskList = [];
+let tabs = document.querySelectorAll(".text-tabs div");
+let mode = 'all';
+let filterList = [];
+
 
 addButton.addEventListener("click", addTask);
 
+for (let i = 1; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", function(event){
+        filter(event);
+    });
+}
+
 
 function addTask() {
+    let taskValue = taskInput.value;
     let task = {
         id: randomIdGenerate(),
-        taskContent: taskInput.value,
+        taskContent: taskValue,
         isComplete: false
     };
     taskList.push(task);
-    console.log(taskList);
+    taskInput.value = "";
     render();
 }
 
 function render() {
-    let resultHTML = '';
-    for (let i = 0; i < taskList.length; i++) {
-        if (taskList[i].isComplete == true) {
+    let resultHTML = "";
+    let list = [];
 
+    if(mode === "all"){
+        list = taskList;
+    }else{
+        list = filterList;
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].isComplete) {
             resultHTML += `<div class="task">
-                            <div class="task-done">${taskList[i].taskContent}</div>
+                            <div class="task-done">${list[i].taskContent}</div>
                             <div>
-                                <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-rotate-right"></i></button>
-                                <button onclick="DeleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
+                                <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-rotate-right"></i></button>
+                                <button onclick="DeleteTask('${list[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
                             </div>
-                        </div>`
+                        </div>`;
         } else {
             resultHTML += `<div class="task">
-                            <div>${taskList[i].taskContent}</div>
+                            <div>${list[i].taskContent}</div>
                             <div>
-                                <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-check"></i></button>
-                                <button onclick="DeleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
+                                <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-check"></i></button>
+                                <button onclick="DeleteTask('${list[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
                             </div>
                         </div>`
         }
@@ -57,10 +67,10 @@ function toggleComplete(id) {
     for (let i = 0; i < taskList.length; i++) {
         if (taskList[i].id == id) {
             taskList[i].isComplete = !taskList[i].isComplete; //반대로 변경
-            render();
             break;
         }
     }
+    filter();
 }
 
 function randomIdGenerate() {
@@ -76,5 +86,30 @@ function DeleteTask(id) {
             taskList.splice(i,1)
         }
     }
-    render(); //반드시 UI도 업데이트 해야함 
+    filter();
+}
+
+function filter(event){
+   if(event){
+    mode = event.target.id //if문 안으로. event없이 호출하는 경우도 있음 
+   } 
+
+   filterList = []; 
+
+     if(mode == "ongoing"){
+        for (let i = 0; i < taskList.length; i++) {
+            if(taskList[i].isComplete == false){
+                filterList.push(taskList[i]);  
+            }
+        }
+    }else if(mode == "done"){
+        for (let i = 0; i < taskList.length; i++) {
+            if(taskList[i].isComplete){
+                filterList.push(taskList[i]);
+            }
+        }
+     
+    }
+
+    render();
 }
